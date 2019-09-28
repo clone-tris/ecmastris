94376b5b96ea059a82db346955ca4989076b6940
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2019-09-28 15:43:13 +0200

    rename entry point to project name

--- src/scripts/Ecmastris.ts
+++ src/scripts/Ecmastris.ts
@@ -1,1 +1,15 @@
-export const Ecmastris = () => {};
+import "../styles/style.css"
+
+import { GameConfig } from "./GameConfig"
+import { Game as GameFactory } from "./framework/Game"
+import { Screen } from "./screens/game/Screen"
+const { CANVAS_WIDTH, CANVAS_HEIGHT } = GameConfig
+
+export const Game = new GameFactory({
+  width: CANVAS_WIDTH,
+  height: CANVAS_HEIGHT,
+  canvas: "#canvas",
+  screen: Screen,
+})
+
+Game.loop()
